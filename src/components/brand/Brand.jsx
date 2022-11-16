d9611feebce4da0e535cbf49aaafb3f77e1df15f
shawncocklin <shawncocklin@gmail.com>
2022-11-15 19:06:30 -0500

    add brand banner

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
-
+import {atlassian, dropbox, google, shopify, slack} from './index'
 import './brand.css'
 
 export default function Brand() {
   return (
-    <div>Brand</div>
+    <div className='brand section-padding'>
+      <div><img src={atlassian} alt="" /></div>
+      <div><img src={dropbox} alt="" /></div>
+      <div><img src={google} alt="" /></div>
+      <div><img src={shopify} alt="" /></div>
+      <div><img src={slack} alt="" /></div>
+    </div>
   )
 }
