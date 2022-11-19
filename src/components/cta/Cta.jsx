1db1c7ad1cb6da6570128641955adcb16c6435cf
shawncocklin <shawncocklin@gmail.com>
2022-11-19 11:06:04 -0500

    desktop cta done

--- src/components/cta/Cta.jsx
+++ src/components/cta/Cta.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import './cta.css'
 
 export default function Cta() {
   return (
-    <div>Cta</div>
+    <div
+      className='cta section-padding section-margin gradient-bar'
+      id='cta'
+    >
+      <div className='cta-body'>
+        <p className='cta-body_caption'>Request Early Access To Get Started</p>
+        <p className='cta-body_message'>
+          Register today & start exploring the endless possiblities.
+        </p>
+      </div>
+      <div className='cta-button'>
+        <button type='button'>Get Started</button>
+      </div>
+    </div>
   )
 }
